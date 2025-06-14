abdd92312f71523873ab924ecbba1a0b45ea1269
catherine su <catherinesu@catherinesmbp3.mynetworksettings.com>
2025-06-14 09:46:38 -0400

    Update Excel file

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from utils import load_data
 import visualize_data as vd
 
 def main():
-    df = load_data("Social Media Analytics.xlsx")
+    df = load_data("Instagram Analytics Sample.xlsx")
     vd.plot_all_metrics(df)
-    vd.plot_one_metric(df, "New Followers")
     
 if __name__ == "__main__":
     main()
